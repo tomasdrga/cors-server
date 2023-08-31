6df65703676671cca72d416026a1425f151e23d3
Tomáš Drga <tomasxdrga@gmail.com>
2023-08-31 12:29:25 +0200

    Edited the whitelist for domains

--- heroku/server.js
+++ heroku/server.js
@@ -8,11 +8,8 @@ const port = process.env.PORT || 3000;
 corsProxy.createServer({
     originWhitelist: [
       'http://localhost:3000',
-      'http://localhost:5000',
-      'https://portfolio-ravenous.web.app',
-      'https://portfolio-ravenous.firebaseapp.com',
-      'https://test-my-api-endpoint.web.app',
-      'https://test-my-api-endpoint.firebaseapp.com'
+      'https://ekosystem.staging.slovensko.digital'
+      'https://ekosystem.slovensko.digital'
     ],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
